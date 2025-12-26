e6c9d90364d1c5c8a737eca96573b27e472e6058
bala-amypo <bala@amypo.in>
2025-12-26 16:49:47 +0000

    User commit

--- src/main/java/com/example/demo/repository/SensorRepository.java
+++ src/main/java/com/example/demo/repository/SensorRepository.java
@@ -1,3 +1,6 @@
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+import java.util.List;
 public interface SensorRepository extends JpaRepository<Sensor, Long> {
     Optional<Sensor> findBySensorCode(String code);
     List<Sensor> findByLocation_Region(String region);
